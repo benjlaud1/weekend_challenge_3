a50d856a8da6f8a4e302a39782f1f0171af5bf0c
Ben Lauderbaugh <benjlaud1@gmail.com>
2017-05-01 00:09:42 -0500

    checkboxes and add working, need to add delete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,70 @@ $(document).ready(onReady);
 // document load function
 function onReady (){
   console.log('jq');
+  $('#add-new-item').on( 'click', addTodoItem );
+  $('#complete-todo-list').on( 'click', '.toggle-complete', toggleItemComplete );
+  // populate the DOM
+  getAllTodoItems();
 } // end onready
+
+// call server to get all items in todo_list database
+function getAllTodoItems () {
+  // make ajax call to the server route /getAllTodoItems
+  $.ajax({
+    url: '/getAllTodoItems',
+    type: 'GET',
+    success: function ( response ) {
+      console.log( 'Back from getAllTodoItems with:', response );
+      // loop through array and append to complete-todo-list
+      $('#complete-todo-list').empty();
+      for (var i = 0; i < response.length; i++) {
+        var todoItem = response[i];
+        $('#complete-todo-list').append( '<nav class="todo-nav"><input type="checkbox" class="toggle-complete" id="toggle-box-' + i + '" data-id="' + todoItem.id + '"><label>' + todoItem.task + '</label></nav>' );
+        if (todoItem.completed) {
+          $('#toggle-box-' + i).prop('checked', true);
+          $('#toggle-box-' + i).parent().addClass( 'completed' );
+        } // end if completed
+      } // end for loop
+    } // end success
+  }); // end ajax GET at /getAllTodoItems
+} // end getAllTodoItems
+
+// add todo item to database
+function addTodoItem () {
+  console.log( 'In addTodoItem' );
+  var objectToAdd = {
+    task: $('#new-todo-item').val()
+  }; // end objectToAdd
+  // make ajax call to the server route /addTodoItem
+  console.log( 'objectToAdd:', objectToAdd );
+  $.ajax({
+    url: '/addTodoItem',
+    type: 'POST',
+    data: objectToAdd,
+    success: function ( response ) {
+      console.log( 'back from addTodoItem with:', response );
+      $('#new-todo-item').val( '' );
+      getAllTodoItems();
+    } // end success
+  }); // end ajax
+} // end addTodoItem
+
+// toggle todo item complete, use /toggleComplete/1/true
+function toggleItemComplete () {
+  console.log( 'In toggleItemComplete' );
+  var objectToToggle = {
+    id: $( this ).data( 'id' ),
+    state: $( this ).prop( 'checked' )
+  }; // end objectToToggle
+  console.log( 'objectToToggle:', objectToToggle );
+  console.log($(this));
+  console.log($(this).parent());
+  $.ajax({
+    url: '/toggleItemComplete/' + objectToToggle.id + '/' + objectToToggle.state,
+    type: 'PUT',
+    success: function ( response ) {
+      console.log( 'Back from toggleItemComplete with:', response );
+      getAllTodoItems();
+    } // end success
+  }); // end ajax
+} // end toggleItemComplete
--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ var pool = new pg.Pool( config );
 
 // globals
 var port = 6789;
+var todoArray = [];
 
 // uses
 app.use( express.static( 'public' ) );
@@ -32,4 +33,65 @@ app.listen( port, function ( req, res ) {
 app.get( '/', function ( req, res ) {
   console.log('Base url hit!');
   res.sendFile( path.resolve( 'public/views/todo.html' ) );
-});
+}); // end base url get
+
+// addTodoItem POST
+app.post( '/addTodoItem', function ( req, res ) {
+  console.log( 'In addTodoItem route' );
+  // connect to DB
+  pool.connect( function ( err, connection, done ) {
+    // check if there's an error
+    if ( err ) {
+      console.log( err );
+      res.sendStatus( 400 );
+    } else {
+      console.log( 'sent to db' );
+      connection.query( "INSERT INTO todo_list_table ( task ) VALUES ( $1 );", [ req.body.task ] );
+      done(); // closes connection
+      res.sendStatus( 201 );
+    } // end else if
+  }); // end pool.connect
+}); // end addTodoItem
+
+// getAllTodoItems GET
+// get all todo items from the databse and push them into an array
+app.get( '/getAllTodoItems', function ( req, res ) {
+  console.log( 'In getAllTodoItems route' );
+  // empty array
+  todoArray = [];
+  pool.connect( function ( err, connection, done ) {
+    if ( err ) {
+      console.log( err );
+      res.sendStatus( 400 );
+    } else {
+      var resultSet = connection.query( "SELECT * FROM todo_list_table ORDER BY id;" );
+      resultSet.on( 'row', function ( row ) {
+        todoArray.push( row );
+      }); // end resultSet on 'row'
+      resultSet.on( 'end', function () {
+        done();
+        console.log( 'Sending todoArray with:', todoArray );
+        res.send( todoArray );
+      }); // end resultSet on end
+    } // end if else
+  }); // end pool connection
+}); // end getAllTodoItems GET
+
+app.put( '/toggleItemComplete/:id/:completed', function ( req, res ) {
+  console.log( 'In toggleItemComplete route' );
+  var objectToToggle = {
+    id: req.params.id,
+    completed: req.params.completed
+  };
+  console.log( 'objectToToggle', objectToToggle );
+  pool.connect( function ( err, connection, done ) {
+    if ( err ) {
+      console.log( err );
+      res.sendStatus( 400 );
+    } else {
+      connection.query( "UPDATE todo_list_table SET completed=$1 WHERE id=$2", [ req.params.completed, req.params.id ] );
+      done();
+      res.sendStatus( 200 );
+    } // end if else
+  }); // end pool.connection
+}); // end toggleItemComplete PUT
